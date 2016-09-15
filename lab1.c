617fa66e45ed723c9c6c38fe5b856541724a2031
Colton <cahxyb@mail.missouri.edu>
2016-09-15 10:30:30 -0500

    first version

---
+++ lab1.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define pi 3.14
+
+int main(void)
+{
+int r;
+printf("Select a radius to calculate the area of a circle for:\n");
+scanf("%d",&r);
+
+float area;
+
+area=pow(r,2)*pi;
+
+printf("the area of the circle is %.2f.\n",area);
+
+return 0;
+}
